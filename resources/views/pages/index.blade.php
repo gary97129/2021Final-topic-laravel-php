cf4567d362ab1283df93663b001991c0881b02d2
gary97129 <40941118@gm.nfu.edu.tw>
2021-12-26 20:26:22 +0800

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('content')
+    <div class="container mt-5">
+        <h1 class="text-center">
+                首頁
+        </h1>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\MyController@get_index_page') -> name('get_index_page');
