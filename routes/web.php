c3e0c202e46beb97c4d03d63a43a11784f37fbfb
dalabonba <upup588@gmail.com>
2021-12-28 21:21:51 +0800

    route 首頁改[ ] 方式

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\MyController@get_index_page') -> name('get_index_page');
+Route::get('/', [App\Http\Controllers\MyController::Class,'get_index_page']) -> name('get_index_page');
