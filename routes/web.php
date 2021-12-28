a18adc1be76c7d2a052f409ca40ffa0267e95f56
dalabonba <upup588@gmail.com>
2021-12-29 00:54:46 +0800

    新增商品route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\MyController::Class,'get_index_page']) -> name('get_index_page');
+
+Route::get('/create',[\App\Http\Controllers\MyController::class,'get_create_page'])->name('get_create_page');
+Route::post('/create',[\App\Http\Controllers\MyController::class,'store_create_item'])->name('store_create_item');
