bcb95244ca54987cbdbccc385d76743df5ad6a47
dalabonba <upup588@gmail.com>
2021-12-29 00:55:50 +0800

    navbar商品新增連結

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="">分頁一<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{route('get_create_page')}}">新增商品<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="">分頁二</a>
