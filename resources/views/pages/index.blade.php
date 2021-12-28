41f02060da824233c72e4992354c21abe5c5164a
gary97129 <40941118@gm.nfu.edu.tw>
2021-12-28 20:53:19 +0800

    更新index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,4 +5,28 @@
                 首頁幹
         </h1>
     </div>
+
+    <article class="bg-secondary py-5 text-center">
+        <!-- footerInfo -->
+        <div class="d-flex justify-content-center align-items-center flex-wrap">
+            <a class="text-light border p-3 m-3" href="https://www.facebook.com/tsvts/" target="_blank">
+                <i class="fab fa-facebook fa-3x"></i><br>泰山職業訓練場
+            </a>
+            <a href="https://tkyhkm.wda.gov.tw/Default.aspx" target="_blank">
+                <img src="https://ws.wda.gov.tw/001/Upload/308/sites/pagebackimage/366d4144-bdac-4a20-a1f2-b0846004efc0.png" alt="勞動部勞動力發展署北基宜花金馬分署全球資訊網">
+            </a>
+            <a href="https://www.taiwanjobs.gov.tw/home/new_index.aspx" target="_blank">
+                <img src="https://icmp-ws.chiayi.gov.tw/001/Upload/408/relpic/9352/408618/8e7e31cb-201c-4b6f-802f-920fb8f06e28@710x470.png" alt="台灣就業通">
+            </a>
+        </div>
+        <div>
+            <a class="text-light" href="tel:02-2901-8274">電話：02-2901-8274</a>　｜　
+            <span class="text-light">傳真：02-2908-4773</span>　｜　
+            <a class="text-light" href="mainto:service@toyugi.com.tw">信箱：service@toyugi.com.tw</a>
+            <address>
+                <a class="text-light" href="https://goo.gl/maps/NDzTUToVSbMQYLwv6" target="_blank">243 新北市泰山區貴子里致遠新村 55 之 1 號</a>
+            </address>
+        </div>
+    </article>
+
 @endsection
