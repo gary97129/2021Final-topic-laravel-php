d60fe742954f348edb9f603754cd07fca70e172b
dalabonba <upup588@gmail.com>
2021-12-30 01:45:22 +0800

    Update create.blade.php
    美化

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,16 +1,28 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="{{route('store_create_item')}}" method="post">
-        @csrf
-        <label>商品名稱</label>
-        <input name="name">
-        <label>商品描述</label>
-        <input name="description">
-        <label>商品圖片路徑</label>
-        <input name="image">
-        <label>商品價格</label>
-        <input name="price">
-        <button type="submit">上傳</button>
-    </form>
+    <div class="container mt-5">
+        <form class="was-validated" action="{{route('store_create_item')}}" method="post">
+            @csrf
+            <div class="form-row justify-content-center">
+                <div class="col-8 mb-5">
+                    <label>商品名稱</label>
+                    <input type="text" class="form-control is-invalid" name="name" required>
+                </div>
+                <div class="col-8 mb-5">
+                    <label>商品描述</label>
+                    <textarea class="form-control" name="description" rows="5"></textarea>
+                </div>
+                <div class="col-8 mb-5">
+                    <label>商品圖片路徑</label>
+                    <input type="text" class="form-control" name="image">
+                </div>
+                <div class="col-4 mb-5">
+                    <label>商品價格</label>
+                    <input type="text" class="form-control is-invalid" name="price" required>
+                </div>
+            </div>
+            <button class="btn btn-outline-info btn-block" type="submit">上傳</button>
+        </form>
+    </div>
 @endsection
