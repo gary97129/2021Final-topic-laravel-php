26470563f31d1922a1250fa7ffa3258adab70bb4
dalabonba <upup588@gmail.com>
2021-12-30 01:47:26 +0800

    Update navbar.blade.php
    黏頂   註冊link

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="{{route('get_index_page')}}">首頁</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,7 +10,7 @@
                 <a class="nav-link" href="{{route('get_create_page')}}">新增商品<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">分頁二</a>
+                <a class="nav-link" href="{{route('get_signup_page')}}">註冊</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
