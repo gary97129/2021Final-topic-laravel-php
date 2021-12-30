1a08e7ed7a511b144d3b27a85fdf9f4a7d1ed729
gary97129 <40941118@gm.nfu.edu.tw>
2021-12-31 01:41:29 +0800

    add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+<article class="bg-secondary py-4 text-center mt-5">
+    <!-- footerInfo -->
+    <div class="d-flex justify-content-center align-items-center flex-wrap">
+        <a class="bg-primary text-light border p-1 m-3" href="https://www.facebook.com" target="_blank" style="width: 2rem;height: 2.5rem">
+            <i class="fa fa-facebook-f" style="font-size:30px"></i>
+        </a>
+
+        <a class="text-light" href="tel:02-2901-8274">電話：02-2901-8274</a>　｜　
+        <span class="text-light">傳真：02-2908-4773</span>　｜　
+        <a class="text-light" href="mainto:service@toyugi.com.tw">信箱：service@toyugi.com.tw</a>
+    </div>
+    <div>
+        <address>
+            <a class="text-light" href="https://goo.gl/maps/NDzTUToVSbMQYLwv6" target="_blank">243 新北市泰山區貴子里致遠新村 55 之 1 號</a>
+        </address>
+    </div>
+</article>
