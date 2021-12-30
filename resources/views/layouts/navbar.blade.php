28c66f0e31437ca3a90d23247c1e80efe382373f
dalabonba <upup588@gmail.com>
2021-12-31 02:01:12 +0800

    Update navbar.blade.php
    登入

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -13,8 +13,11 @@
             </li>
         </ul>
         <ul class="navbar-nav my-2 my-lg-0 mr-3">
-            <li class="nav-item">
+            <li class="nav-item mr-2">
                 <a class="btn btn-outline-info" href="{{route('get_signup_page')}}" role="button">註冊</a>
+            </li>
+            <li class="nav-item">
+                <a class="btn btn-outline-danger" href="{{route('get_signin_page')}}" role="button">登入</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
