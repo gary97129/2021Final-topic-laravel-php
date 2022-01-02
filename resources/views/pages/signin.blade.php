cc5305d6c9806ab500d9c0584898b2f4d7a4df10
gary97129 <40941118@gm.nfu.edu.tw>
2022-01-02 14:56:56 +0800

    登入外觀

--- resources/views/pages/signin.blade.php
+++ resources/views/pages/signin.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container mt-5">
-        <form class="was-validated mb-5" action="{{route('signin_go')}}" method="post">
-            @csrf
-            <div class="form-row justify-content-center">
-                <div class="col-8 mb-5">
-                    <label>帳號</label>
-                    <input type="text" class="form-control is-invalid" name="account" required>
-                </div>
-                <div class="col-8 mb-5">
-                    <label>密碼</label>
-                    <input type="password" class="form-control is-invalid" name="password" required>
-                    @if($not_match)
-                        <div class="invalid-feedback">
-                            帳號或密碼錯誤
+
+    <div class="container" style="margin-top:100px;margin-bottom: 100px">
+        <div class="card bg-light" style="max-width: 25rem;margin:0 auto;">
+            <h1 class="text-center card-header">登入</h1>
+            <div class="card-body">
+                <form class="was-validated mb-5" action="{{route('signin_go')}}" method="post">
+                    @csrf
+                    <div class="justify-content-center">
+                        <div class="mb-5">
+                            <label>帳號</label>
+                            <input type="text" class="form-control is-invalid" name="account" required>
                         </div>
-                    @endif
-                </div>
+                        <div class="mb-5">
+                            <label>密碼</label>
+                            <input type="password" class="form-control is-invalid" name="password" required>
+                            @if($not_match)
+                                <div class="invalid-feedback">
+                                    帳號或密碼錯誤
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                    <button class="btn btn-outline-info btn-block" type="submit">登入</button>
+                </form>
             </div>
-            <button class="btn btn-outline-info btn-block" type="submit">登入</button>
-        </form>
+        </div>
     </div>
 @endsection
